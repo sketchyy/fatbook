edbdb49f4d2a0423459a7e79907faeb55906430a
Sketchyy <sketchyy@ya.ru>
2022-12-14 01:06:35 +0100

    Dish form initial rendering

--- src/routes/dish-form/DishForm.jsx
+++ src/routes/dish-form/DishForm.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { FaSave } from "react-icons/fa";
 import { useLoaderData } from "react-router-dom";
 
 export async function dishLoader({ params }) {
@@ -15,7 +16,75 @@ export async function dishLoader({ params }) {
 function DishForm(props) {
   const contact = useLoaderData();
 
-  return <div>dish form {contact.name}</div>;
+  return (
+    <Fragment>
+      <div className="tabs is-boxed is-centered mb-0">
+        <ul>
+          <li className="is-active">
+            <a>Dish</a>
+          </li>
+          <li>
+            <a>Ingredients</a>
+          </li>
+        </ul>
+      </div>
+      <div className="box">
+        <div className="field">
+          <label className="label">Name</label>
+          <div className="control">
+            <input className="input" type="text" />
+          </div>
+        </div>
+        <div className="field is-grouped">
+          <div className="field mr-3">
+            <label className="label">Proteins</label>
+            <div className="control">
+              <input className="input" type="text" placeholder="per 100g." />
+            </div>
+          </div>
+          <div className="field mr-3">
+            <label className="label">Fats</label>
+            <div className="control">
+              <input className="input" type="text" placeholder="per 100g." />
+            </div>
+          </div>
+          <div className="field">
+            <label className="label">Carbs</label>
+            <div className="control">
+              <input className="input" type="text" placeholder="per 100g." />
+            </div>
+          </div>
+        </div>
+
+        <div className="field is-grouped is-grouped-centered">
+          <div className="field mr-3">
+            <label className="label">KCal</label>
+            <div className="control">
+              <input className="input" type="text" placeholder="per 100g." />
+            </div>
+          </div>
+          <div className="field">
+            <label className="label">Portion Size</label>
+            <div className="control">
+              <input className="input" type="text" placeholder="gramms" />
+            </div>
+          </div>
+        </div>
+
+        <div className="field is-grouped is-grouped-centered is-justify-content-space-around">
+          <p className="control">
+            <button className="button is-light">Cancel</button>
+          </p>
+          <p className="control">
+            <button className="button is-primary">
+              <FaSave className="mr-2" />
+              Save
+            </button>
+          </p>
+        </div>
+      </div>
+    </Fragment>
+  );
 }
 
 export default DishForm;
