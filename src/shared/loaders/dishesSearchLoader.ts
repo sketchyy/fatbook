da2702fffe78f65f5a1e398d7f44a62c49508ae6
Sketchyy <sketchyy@ya.ru>
2024-02-19 22:58:33 +0100

    Sort by updatedAt

--- src/shared/loaders/dishesSearchLoader.ts
+++ src/shared/loaders/dishesSearchLoader.ts
@@ -4,7 +4,10 @@ import Dish from "@/shared/models/Dish";
 export async function dishesSearchLoader({ request }) {
   const url = new URL(request.url);
   const q = url.searchParams.get("q");
-  const { data, error } = await supabase.from("dishes").select();
+  const { data, error } = await supabase
+    .from("dishes")
+    .select()
+    .order("updatedAt", { ascending: false });
 
   return {
     data: data!.map((d) => Dish.fromSupabase(d)),
