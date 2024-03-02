0ced6a68f33c4c0e1f0ffc537fbb411626d68cb4
Sketchyy <sketchyy@ya.ru>
2024-03-02 18:07:05 +0100

    Add cypress test to create dish

--- cypress/e2e/dishes.cy.ts
+++ cypress/e2e/dishes.cy.ts
@@ -2,11 +2,19 @@ describe("dishes", () => {
   beforeEach(() => {
     cy.loginAsTestUser();
 
-    cy.visit("/dishes");
+    cy.contains("Dishes").click();
   });
 
   it("should create new dish", () => {
+    // create 3 test dishes
     cy.contains("New").click();
+    cy.get('input[name="name"]').should("be.visible").should("be.enabled");
+    cy.get('input[name="name"]').type("test_hamburger");
+    cy.get('input[name="foodValue.proteins"]').type("20");
+    cy.get('input[name="foodValue.fats"]').type("30");
+    cy.get('input[name="foodValue.carbs"]').type("40");
+    cy.get('input[name="foodValue.calories"]').type("300");
+    cy.contains("Save").click();
   });
 
   it.skip("should create new dish with ingredients", () => {
