559191035842eccfadc3d579aa3c9eb41b9e76fd
Sketchyy <sketchyy@ya.ru>
2024-04-28 11:27:27 +0200

    feat: add optimistic updates for eatings

--- src/hooks/use-eating-mutations.ts
+++ src/hooks/use-eating-mutations.ts
@@ -11,40 +11,101 @@ import {
 } from "@/services/eatings-service";
 import { useParams } from "react-router-dom";
 import { useAuth } from "@/context/Auth";
+import { useState } from "react";
+import { calculateFoodValue } from "@/utils/food-value-utils";
+import { toast } from "react-toastify";
+
+type EatingOnMutate = (portion: DishPortion) => void;
 
 type UseEatingMutations = {
   add: UseMutationResult<DishPortion, unknown, DishPortion>;
   update: UseMutationResult<DishPortion, unknown, DishPortion>;
   remove: UseMutationResult<void, unknown, DishPortion>;
+  selectedPortions: DishPortion[];
 };
 
 export function useEatingMutations(meal: string): UseEatingMutations {
   const queryClient = useQueryClient();
   const { day } = useParams();
   const { userId } = useAuth();
+  const [selectedPortions, setSelectedPortions] = useState<DishPortion[]>([]);
 
-  const onSuccess = () =>
-    queryClient.invalidateQueries({ queryKey: ["dailyEatings", day] });
+  // Optimistic update
+  const createOnMutate = (actualMutation: EatingOnMutate) => (portion) => {
+    // Perform optimistic update
+    actualMutation(portion);
+    // Snapshot the previous value (will be used in case of error)
+    return { previousValue: selectedPortions.slice() };
+  };
+  // Write actual value from the response
+  const onSuccess = (response) => {
+    setSelectedPortions((portions) => {
+      const optimisticIndex = portions.findIndex(
+        // Only one dish can be added at a time, hence dish id is unique
+        (p) => p.dish.id === response.dish.id,
+      );
+      portions[optimisticIndex] = response;
+      return portions;
+    });
+    queryClient.invalidateQueries({ queryKey: makeKey(day) });
+  };
+  // const onSettled = () => {
+  //   queryClient.invalidateQueries({ queryKey: key(day) });
+  // };
+  // Revert to previous value if error occured
+  const onError = (_err, _newTodo, context) => {
+    toast.error("Error while saving");
+    setSelectedPortions(context!.previousValue);
+  };
 
   const add = useMutation({
     mutationFn: (portion: DishPortion) =>
       createEating(userId, day!, meal!, portion),
+    onMutate: createOnMutate((newPortion) => {
+      newPortion.selected = true;
+      const foodValue = calculateFoodValue(newPortion);
+      setSelectedPortions((portions) => [
+        ...portions,
+        { ...newPortion, ...foodValue },
+      ]);
+    }),
     onSuccess,
+    onError,
   });
 
   const update = useMutation({
     mutationFn: (portion: DishPortion) => updateEating(portion),
+    onMutate: createOnMutate((updatedPortion) => {
+      setSelectedPortions((portions) => {
+        const foodValue = calculateFoodValue(updatedPortion);
+        const idx = portions.findIndex(
+          (p) => p.dish.id === updatedPortion.dish.id,
+        );
+        portions[idx] = { ...updatedPortion, ...foodValue };
+        return portions;
+      });
+    }),
     onSuccess,
+    onError,
   });
 
   const remove = useMutation({
     mutationFn: (portion: DishPortion) => deleteEating(portion),
+    onMutate: createOnMutate((deletedPortion) => {
+      setSelectedPortions((portions) => {
+        return portions.filter((p) => p.dish.id !== deletedPortion.dish.id);
+      });
+    }),
     onSuccess,
+    onError,
   });
 
   return {
     add,
     update,
     remove,
+    selectedPortions,
   };
 }
+
+const makeKey = (day?: string) => ["dailyEatings", day];
--- src/pages/eatings/EatingsAdd.tsx
+++ src/pages/eatings/EatingsAdd.tsx
@@ -2,40 +2,22 @@ import SelectDishPortionsForm from "@/components/dish-portions-form/SelectDishPo
 import { isToday, isYesterday } from "@/utils/date-utils";
 import { useParams } from "react-router-dom";
 import { DishPortion } from "@/types/dish-portion";
-import { useState } from "react";
 import { useEatingMutations } from "@/hooks/use-eating-mutations";
 
 function EatingsAdd() {
   const { day, meal } = useParams();
-  const [selectedPortions, setSelectedPortions] = useState<DishPortion[]>([]);
-  const { add, update, remove } = useEatingMutations(meal!);
+  const { add, update, remove, selectedPortions } = useEatingMutations(meal!);
 
   const handleAddEating = async (portion: DishPortion) => {
-    add.mutate(portion, {
-      onSuccess: (response) => {
-        setSelectedPortions((portions) => [...portions, response]);
-      },
-    });
+    add.mutate(portion);
   };
 
   const handleUpdateEatings = async (portion: DishPortion) => {
-    update.mutate(portion, {
-      onSuccess: (response) => {
-        setSelectedPortions((portions) =>
-          portions.map((p) => (p.id === response.id ? response : p)),
-        );
-      },
-    });
+    update.mutate(portion);
   };
 
   const handleDeleteEatings = async (portion: DishPortion) => {
-    remove.mutate(portion, {
-      onSuccess: () => {
-        setSelectedPortions((portions) =>
-          portions.filter((p) => p.id !== portion.id),
-        );
-      },
-    });
+    remove.mutate(portion);
   };
 
   const getSubtitle = () => {
