2130a3be3681c7db95e822ac1d24028ccd4fcadc
Sketchyy <sketchyy@ya.ru>
2024-05-11 11:56:25 +0200

    feat: Don't render loading animation when using optimistic UI

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -4,7 +4,7 @@ import { useIsFetching } from "@tanstack/react-query";
 import { clsx } from "clsx";
 
 function Root() {
-  const fetchingCount = useIsFetching();
+  const fetchingCount = useIsFetching({ stale: false });
   const isLoading = fetchingCount > 0;
 
   return (
