7d8ef01927487edfdebb6334ccce0218e3d95f08
Sketchyy <sketchyy@ya.ru>
2024-09-22 14:39:38 +0200

    Fix mobile issues 2

--- src/components/FoodValue.tsx
+++ src/components/FoodValue.tsx
@@ -2,6 +2,7 @@ import { FoodValue as FoodValueType } from "@/types/food-value";
 import { emptyFoodValue } from "@/utils/food-value-utils";
 import { clsx } from "clsx";
 import { Level } from "@/components/ui/Level";
+import { useIsTouchDevice } from "@/hooks/use-is-touch-device";
 
 type Props = {
   source?: FoodValueType | null;
@@ -13,10 +14,12 @@ function FoodValue({ source, className = "", isLoading }: Props) {
   // TODO: single format func
   const format = (val: number) => (val != null ? Math.round(val) : "n/a");
   const rendered = source ?? emptyFoodValue();
+  const isTouchDevice = useIsTouchDevice();
 
   const cls = clsx("tag", {
     "is-skeleton px-1": isLoading,
     "has-background-none px-0": !isLoading,
+    "is-unselectable": isTouchDevice,
   });
 
   return (
